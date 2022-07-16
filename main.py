317bb265cc694a91127e37eadc893cd789b2534c
devferx <ferkki@hotmail.com>
2022-07-16 17:28:28 -0400

    automatic_path_and_query_examples

--- main.py
+++ main.py
@@ -100,11 +100,13 @@ def show_person(
         max_length = 50,
         title="Person Name",
         description="This is the person name. It's between 1 and 50 characters",
+        example="Rocio"
         ),
     age: str = Query(
         ...,
         title = "Person Age",
-        description="This is the person age. It's required"
+        description="This is the person age. It's required",
+        example=25
         )
 ):
     return {"name": name, "age": age}
@@ -116,7 +118,8 @@ def show_person(
         ...,
         gt=0,
         title="Person ID",
-        description="This is the person id. It's required"
+        description="This is the person id. It's required",
+        example=123
         )
 ):
     return {person_id: "It exists!"}
@@ -128,7 +131,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
     # location: Location = Body(...),
